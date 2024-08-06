ec57bfa92bb8314b3cb27babc802fdb24bcb180b
yip99 <49900712+yip99@users.noreply.github.com>
2024-08-06 05:01:52 +0000

    .

--- index.js
+++ index.js
@@ -1,20 +1,3 @@
-class Choices extends Map {
-    first() {
-        return super.entries().next().value;
-    }
-    last() {
-        return [...super.entries()].at(-1);
-    }
-    add(key, value) {
-        if (super.has(key)) {
-            super.get(key).number += (+value.number);
-        } else {
-            super.set(key, value);
-            this.lastItem = [key, super.get(key)];
-        }
-    }
-}
-
 const numberFormat = new Intl.NumberFormat('en-US', {
     maximumFractionDigits: 2,
 });
@@ -40,6 +23,39 @@ const inputNumber = document.querySelector('input[type="number"]');
 const inputText = document.querySelector('input[name="choice-text"]');
 const buttonSpin = document.querySelector('#spin');
 const elementResults = document.querySelector('.results');
+const inputColor = document.querySelector('input[name="choice-color"]');
+const listContainer = document.querySelector('.list-choices-container');
+document.querySelector('#results-copy')?.addEventListener('click', () => {
+    if (results.length === 0) {
+        return;
+    }
+    navigator.clipboard.writeText(results.join('\n'));
+});
+document.querySelector('#results-clear')?.addEventListener('click', () => {
+    results.splice(0, results.length);
+    displayResult();
+});
+document.querySelector('#choices-import')?.addEventListener('click', async () => {
+    const clipboard = JSON.parse(await navigator.clipboard.readText());
+    choices.splice(0, choices.length);
+    choices.push(...clipboard);
+    updateChoice();
+});
+document.querySelector('#choices-export')?.addEventListener('click', () => {
+    navigator.clipboard.writeText(JSON.stringify(choices, null, '\t'));
+});
+document.querySelector('#choices-copy')?.addEventListener('click', () => {
+    if (choices.length === 0) {
+        return;
+    }
+    navigator.clipboard.writeText(choices.map(choice => choice.text).join('\n'));
+});
+document.querySelector('#choices-clear')?.addEventListener('click', () => {
+    choices.splice(0, choices.length);
+    updateChoice();
+});
+
+inputColor.value = colors[Math.floor(Math.random() * colors.length)];
 
 const arrowAngle = +getComputedStyle(document.body).getPropertyValue('--arrow-angle').match(/rotate\(([+-]?\d+)deg\)/)?.[1];
 
@@ -60,8 +76,7 @@ let startSpeed;
 let stopSpeed;
 let speed = 0;
 
-let options = [];
-const choices = new Choices();
+const choices = [];
 const results = [];
 
 
@@ -70,11 +85,11 @@ let angle;
 
 window.addEventListener('resize', function () {
     radius = elementRouletteChoices.getBoundingClientRect().width / 2;
-    updateOption();
+    updateChoice();
 }, true);
 
 buttonSpin.addEventListener("click", () => {
-    if (choices.size < 2) {
+    if (choices.length < 2) {
         return;
     }
     buttonSpin?.classList.toggle('spinning');
@@ -111,12 +126,12 @@ document.querySelector('#decrement').addEventListener('click', () => {
 });
 
 document.querySelector('#add').addEventListener('click', () => {
-    addOption();
+    addChoice();
 });
 
 inputText?.addEventListener('keypress', (event) => {
     if (event.key === 'Enter') {
-        addOption();
+        addChoice();
     }
 });
 
@@ -136,7 +151,6 @@ function startSpin() {
 }
 
 function spin() {
-    // console.log('spin');
     elapsed = Date.now() - then;
     fps = 1000 / elapsed;
     then = Date.now();
@@ -160,7 +174,7 @@ function stopSpin() {
         const result = getResult();
         results.push(result);
         displayResult();
-        console.log({ results, result });
+        elementResults.scrollTop = elementResults.scrollHeight;
     }
 }
 
@@ -176,35 +190,40 @@ function y(cy, radius, degree) {
     return cy - (radius * Math.cos(degree * (Math.PI / 180)));
 }
 
-function addOption() {
+function addChoice() {
+    if (inputText.value === '') {
+        return;
+    }
     const choice = {
+        text: inputText.value,
         number: +inputNumber.value,
-        color: colors[Math.floor(Math.random() * colors.length)],
+        color: inputColor.value,
     }
-    if (!choices.has(inputText.value)) {
-        const firstChoice = choices.first()?.[1];
-        const lastChoice = choices.last()?.[1];
-        while ([firstChoice?.color, lastChoice?.color].includes(choice.color)) {
-            choice.color = colors[Math.floor(Math.random() * colors.length)];
-        }
+
+    choices.push(choice);
+    updateChoice();
+    listContainer.scrollTop = listContainer.scrollHeight;
+
+    const lastChoice = choices.at(-1);
+    let newColor = colors[Math.floor(Math.random() * colors.length)];
+    while (lastChoice?.color === newColor) {
+        newColor = colors[Math.floor(Math.random() * colors.length)];
     }
-    choices.add(inputText.value, choice);
-    updateOption();
+    inputColor.value = newColor;
     inputText.value = '';
     inputNumber.value = 1;
     inputText.focus();
 }
 
-function updateOption() {
+function updateChoice() {
     elementListChoices.innerHTML = '';
     elementRouletteChoices.innerHTML = '';
-    angle = 360 / [...choices.values()].reduce((total, obj) => obj.number + total, 0);
+    angle = 360 / choices.reduce((total, obj) => obj.number + total, 0);
     let degreeFrom = 0;
-    // rotation = (180 - angle) / 2;
     elementRouletteChoices.style.transform = `rotate(${rotation}deg)`;
     let no = 1;
-    choices.forEach(({ number, color }, text) => {
-        const degreeTo = number * angle;
+    for (let i = 0; i < choices.length; i++) {
+        const degreeTo = choices[i].number * angle;
         const div = document.createElement('div');
         div.className = 'roulette-choice';
         div.style.transform = `rotate(${degreeFrom}deg)`;
@@ -213,15 +232,16 @@ function updateOption() {
         svg.setAttribute('width', `${radius * 2}`);
         svg.setAttribute('height', `${radius * 2}`);
         svg.appendChild(path)
-        path.setAttribute('fill', `${color}`);
+        path.setAttribute('fill', `${choices[i].color}`);
         path.setAttribute(
             'd',
-            choices.size === 1
+            choices.length === 1
                 ? `M ${radius} ${radius} M ${radius * 2} ${radius} A ${radius} ${radius} 0 1 0 0 ${radius} A ${radius} ${radius} 0 1 0 ${radius * 2} ${radius}`
                 : `M ${radius} ${radius} L ${radius} 0 A ${radius} ${radius} 0 ${degreeTo > 180 ? '1' : '0'} 1 ${x(radius, radius, degreeTo)} ${y(radius, radius, degreeTo)} Z`
         );
         const span = document.createElement('span');
-        span.textContent = text;
+        span.textContent = choices[i].text;
+        span.style.color = getContrastYIQ(choices[i].color);
         span.style.transform = `rotate(${degreeTo < 360 ? (degreeTo - 180) / 2 : 0}deg)`;
         div.appendChild(svg);
         div.appendChild(span);
@@ -229,49 +249,71 @@ function updateOption() {
         degreeFrom += degreeTo;
 
         const listChoice = document.createElement('div');
+        const listChoiceColor = document.createElement('input');
         const listChoiceNo = document.createElement('span');
-        const listChoiceText = document.createElement('span');
+        const listChoiceText = document.createElement('input');
         const listChoiceDecrement = document.createElement('button');
         const listChoiceNumber = document.createElement('input');
         const listChoiceIncrement = document.createElement('button');
         const listChoicePercentage = document.createElement('span');
         const listChoiceRemove = document.createElement('button');
 
+        listChoiceColor.type = 'color';
+        listChoiceColor.value = choices[i].color;
+        listChoiceColor.addEventListener('input', (event) => {
+            choices[i].color = event.currentTarget.value;
+            path.setAttribute('fill', `${choices[i].color}`);
+            span.style.color = getContrastYIQ(choices[i].color);
+        });
         listChoice.className = 'list-choice';
         listChoiceNo.className = 'number';
         listChoiceNo.textContent = `${no++}.`;
         listChoiceText.className = 'list-choice-text';
-        listChoiceText.textContent = text;
-        listChoiceText.title = text;
+        listChoiceText.value = choices[i].text;
+        listChoiceText.title = choices[i].text;
+        listChoiceText.addEventListener('input', (event) => {
+            choices[i].text = event.currentTarget.value;
+            span.textContent = choices[i].text;
+            listChoiceText.title = choices[i].text;
+        });
         listChoiceDecrement.textContent = '-';
         listChoiceDecrement.addEventListener('click', () => {
-            if (number < 2) {
+            if (choices[i].number < 2) {
                 return;
             }
-            choices.get(text).number--;
-            updateOption();
+            choices[i].number--;
+            updateChoice();
         });
         listChoiceNumber.type = 'number';
-        listChoiceNumber.value = `${number}`;
+        listChoiceNumber.value = `${choices[i].number}`;
         listChoiceIncrement.textContent = '+';
         listChoiceIncrement.addEventListener('click', () => {
-            choices.get(text).number++;
-            updateOption();
+            choices[i].number++;
+            updateChoice();
         });
 
         listChoicePercentage.className = 'list-choice-percentage';
-        listChoicePercentage.textContent = `${numberFormat.format((angle / 360 * number) * 100)}%`;
+        listChoicePercentage.textContent = `${numberFormat.format((angle / 360 * choices[i].number) * 100)}%`;
 
         listChoiceRemove.className = 'remove';
         listChoiceRemove.textContent = 'Remove';
         listChoiceRemove.addEventListener('click', () => {
-            choices.delete(text);
-            updateOption();
+            choices.splice(i, 1);
+            updateChoice();
         });
 
-        listChoice.append(listChoiceNo, listChoiceText, listChoiceDecrement, listChoiceNumber, listChoiceIncrement, listChoicePercentage, listChoiceRemove);
+        listChoice.append(
+            listChoiceColor,
+            listChoiceNo,
+            listChoiceText,
+            listChoiceDecrement,
+            listChoiceNumber,
+            listChoiceIncrement,
+            listChoicePercentage,
+            listChoiceRemove
+        );
         elementListChoices.appendChild(listChoice);
-    });
+    }
 }
 
 function getResult() {
@@ -279,14 +321,13 @@ function getResult() {
     let from = 360;
     let to = 360;
     if (theta === 0) {
-        return choices.first()?.[0];
+        return choices[0];
     }
-    for (let [text, { number }] of choices) {
+    for (let i = 0; i < choices.length; i++) {
         to = from;
-        from -= number * angle;
-        console.log((360 + theta - arrowAngle) % 360, { theta: (360 + theta - arrowAngle) % 360, text, from, to });
+        from -= choices[i].number * angle;
         if ((360 + theta - arrowAngle) % 360 >= from && (360 + theta - arrowAngle) % 360 < to) {
-            return text;
+            return choices[i].text;
         }
     }
 }
@@ -306,4 +347,12 @@ function displayResult() {
         div.appendChild(span);
         elementResults?.appendChild(div);
     }
+}
+
+function getContrastYIQ(hexcolor) {
+    var r = parseInt(hexcolor.substring(1, 3), 16);
+    var g = parseInt(hexcolor.substring(3, 5), 16);
+    var b = parseInt(hexcolor.substring(5, 7), 16);
+    var yiq = ((r * 299) + (g * 587) + (b * 114)) / 1000;
+    return (yiq >= 128) ? 'black' : 'white';
 }
